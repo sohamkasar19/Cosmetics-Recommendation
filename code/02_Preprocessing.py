9da7822d220a659c1f2052ee41dd7ce8253bcd2d
Shrisha <shrishashegde02@gmail.com>
2022-05-03 15:46:20 -0700

    Added function to preprocess the ingredients column

--- code/02_Preprocessing.py
+++ code/02_Preprocessing.py
@@ -10,6 +10,20 @@ def clean_data(df):
     df.drop(columns=['URL'], inplace=True)
 
 
+def preprocess_ingredients(df):
+    df.dropna(subset=['ingredients'], inplace=True)
+    processed_ingredient = [t.split('\n') for t in df['ingredients']]
+    pattern = ['\r\n', 'Please', 'No Info', 'This product', 'Visit']
+
+    for i in range(len(df)):
+        num = len(processed_ingredient[i])
+        for j in range(num):
+            if all(x not in processed_ingredient[i][j] for x in pattern):
+                df['ingredients'][i] = processed_ingredient[i][j]
+    return df
+
+
 if __name__ == '__main__':
-    df = pd.read_csv('data/cosmetic.csv')
+    df = pd.read_csv('data/cosmetic.csv', na_values={'NA', '#NAME?'})
     clean_data(df)
+    df = preprocess_ingredients(df)
